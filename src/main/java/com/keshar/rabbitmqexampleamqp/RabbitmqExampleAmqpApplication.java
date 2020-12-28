e137da8aabc750082bf6256faa09bd2499e4a445
keshar <paudelkeshar2055@gmail.com>
2020-12-28 08:42:49 +0545

    Initialize project

---
+++ src/main/java/com/keshar/rabbitmqexampleamqp/RabbitmqExampleAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.keshar.rabbitmqexampleamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqExampleAmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqExampleAmqpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keshar/rabbitmqexampleamqp/RabbitmqExampleAmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keshar.rabbitmqexampleamqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqExampleAmqpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
